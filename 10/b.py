5a3db7c5fe8d5d96eda5833d51affbba1aa66c69
Marco Schlicht <git@privacymail.dev>
2021-12-10 20:46:19 +0100

    refactored code

--- 10/b.py
+++ 10/b.py
@@ -1,190 +1,17 @@
-import ply.lex as lex
-import ply.yacc as yacc
-from functools import reduce
+from parser import *
 
 
 FILENAME = "input"
 LINT = True
 
 
-def find_column(input, token):
-    line_start = input.rfind('\n', 0, token.lexpos) + 1
-    return (token.lexpos - line_start) + 1
-
-
-# LEXER
-
-tokens = [
-    'NEWLINE',
-    'LROUND',
-    'RROUND',
-    'LCURLY',
-    'RCURLY',
-    'LSQUARE',
-    'RSQUARE',
-    'LANGLE',
-    'RANGLE',
-]
-
-t_LROUND = r'\('
-t_RROUND = r'\)'
-t_LCURLY = r'{'
-t_RCURLY = r'}'
-t_LSQUARE = r'\['
-t_RSQUARE = r'\]'
-t_LANGLE = r'<'
-t_RANGLE = r'>'
-
-
-def t_NEWLINE(t):
-    r'\n+'
-    t.lexer.lineno += len(t.value)
-    return t
-
-
-def t_error(t):
-    column = find_column(t.lexer.lexdata, t)
-    msg = f"Illegal character: {repr(t.value[0])}"
-    form = "{path}:{line}:{column}: ({symbol}) {msg}"
-    formatted = form.format(path=FILENAME, line=t.lineno, column=column, symbol=t.type, msg=msg)
-    if LINT:
-        print(formatted)
-        t.lexer.skip(1)
-    else:
-        raise Exception(formatted)
-
-
-lexer = lex.lex()
-
-
-def do_lexing(data):
-    lexer.input(data)
-    while True:
-        i = lexer.token()
-        if i is None:
-            break
-        print(i)
-
-
-# PARSER
-
-start = 'codeblock'
-
-
-def p_program1(p):
-    '''codeblock : line'''
-    line = p[1]
-    p[0] = [line]
-
-
-def p_program2(p):
-    '''codeblock : codeblock line'''
-    codeblock = p[1]
-    line = p[2]
-    p[0] = codeblock + [line]
-
-
-def p_line(p):
-    '''line : chunks NEWLINE'''
-    chunks = p[1]
-    p[0] = chunks
-
-
-def p_chunks(p):
-    '''chunks : chunk'''
-    p[0] = p[1]
-
-
-def p_chunks2(p):
-    '''chunks : chunks chunk'''
-    p[0] = p[1] + p[2]
-
-
-def p_chunk_error(p):
-    '''chunks : error chunk
-    '''
-    p[0] = p[2]
-    p.parser.errok()
-
-
-def p_chunk1(p):
-    '''chunk : LROUND RROUND
-             | LSQUARE RSQUARE
-             | LANGLE RANGLE
-             | LCURLY RCURLY
-    '''
-    p[0] = p[1] + p[2]
-
-
-def p_chunk2(p):
-    '''chunk : LROUND chunks RROUND
-             | LSQUARE chunks RSQUARE
-             | LANGLE chunks RANGLE
-             | LCURLY chunks RCURLY
-    '''
-    p[0] = p[1] + p[2] + p[3]
-
-
-precedence = (
-    ('left', 'LROUND'),
-    ('left', 'RROUND'),
-    ('left', 'LSQUARE'),
-    ('left', 'RSQUARE'),
-    ('left', 'LANGLE'),
-    ('left', 'RANGLE'),
-    ('left', 'LCURLY'),
-    ('left', 'RCURLY'),
-    ('left', 'NEWLINE'),
-)
-
-
-chunk_errors = []
-
-
-incomplete = []
-corrupted = []
-# should be: 3, 5, 6, 8, 9
-
-
-def p_error(p):
-    if p is None:
-        raise Exception("Unexpected end of file.")
-    msg = f"Syntax error! Error on token: {repr(p.value)} ({p.type})"
-    make_error(p, "error", msg)
-
-
-def make_error(p, type, message):
-    column = find_column(p.lexer.lexdata, p)
-    form = "{path}:{line}:{column}: {type}: {msg}"
-    formatted = form.format(path=FILENAME, line=p.lineno, column=column, type=type, msg=message)
-    if p.type == "NEWLINE":
-        incomplete.append((p.lineno, column, p.type, [x for x in parser.symstack]))
-        parser.restart()
-    else:
-        corrupted.append((p.lineno, column, p.type))
-    if LINT:
-        print(formatted)
-    else:
-        raise Exception(formatted)
-
-
-parser = yacc.yacc()
-
-
-def do_parsing(text):
-    result = parser.parse(text)
-    return result
-
-
-def main(filename):
-    data = open(filename).read()
-    lines = open(filename).read().splitlines()
-    # do_lexing(data)
+if __name__ == '__main__':
+    data = open(FILENAME).read()
+    setup(LINT, FILENAME)
     do_parsing(data)
-    # reverse order to overwrite with the first item at last
     point_map = {'LROUND': 1, 'LSQUARE': 2, 'LCURLY': 3, 'LANGLE': 4}
-    corrupted_first = {x: (y, z) for x, y, z in corrupted[::-1]}
-    only_incompletes = [x for x in incomplete if x[0] not in corrupted_first.keys()]
+    corrupted_first = [x for x, _, _ in corrupted]
+    only_incompletes = [x for x in incomplete if x[0] not in corrupted_first]
     points = []
     for only_incomplete in only_incompletes:
         line_points = 0
@@ -195,12 +22,3 @@ def main(filename):
                 line_points += point_map[x.type]
         points.append(line_points)
     print(sorted(points)[len(points) // 2])
-
-
-if __name__ == '__main__':
-    main(FILENAME)
-
-
-# 3 curly ...
-# 3 square ...
-# 2 round ..
---
+++ 10/parser.py
@@ -0,0 +1,172 @@
+import ply.lex as lex
+import ply.yacc as yacc
+
+
+FILENAME = ""
+LINT = False
+
+
+def setup(lint: bool, filename):
+    global LINT, FILENAME
+    LINT = lint
+    FILENAME = filename
+
+
+def find_column(input, token):
+    line_start = input.rfind('\n', 0, token.lexpos) + 1
+    return (token.lexpos - line_start) + 1
+
+
+# LEXER
+
+tokens = [
+    'NEWLINE',
+    'LROUND',
+    'RROUND',
+    'LCURLY',
+    'RCURLY',
+    'LSQUARE',
+    'RSQUARE',
+    'LANGLE',
+    'RANGLE',
+]
+
+t_LROUND = r'\('
+t_RROUND = r'\)'
+t_LCURLY = r'{'
+t_RCURLY = r'}'
+t_LSQUARE = r'\['
+t_RSQUARE = r'\]'
+t_LANGLE = r'<'
+t_RANGLE = r'>'
+
+
+def t_NEWLINE(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+    return t
+
+
+def t_error(t):
+    column = find_column(t.lexer.lexdata, t)
+    msg = f"Illegal character: {repr(t.value[0])}"
+    form = "{path}:{line}:{column}: ({symbol}) {msg}"
+    formatted = form.format(path=FILENAME, line=t.lineno, column=column, symbol=t.type, msg=msg)
+    if LINT:
+        print(formatted)
+        t.lexer.skip(1)
+    else:
+        raise Exception(formatted)
+
+
+lexer = lex.lex()
+
+
+# PARSER
+
+start = 'codeblock'
+
+
+def p_program1(p):
+    '''codeblock : line'''
+    line = p[1]
+    p[0] = [line]
+
+
+def p_program2(p):
+    '''codeblock : codeblock line'''
+    codeblock = p[1]
+    line = p[2]
+    p[0] = codeblock + [line]
+
+
+def p_line(p):
+    '''line : chunks NEWLINE'''
+    chunks = p[1]
+    p[0] = chunks
+
+
+def p_chunks(p):
+    '''chunks : chunk'''
+    p[0] = p[1]
+
+
+def p_chunks2(p):
+    '''chunks : chunks chunk'''
+    p[0] = p[1] + p[2]
+
+
+def p_chunk_error(p):
+    '''chunks : error chunk
+    '''
+    p[0] = p[2]
+    p.parser.errok()
+
+
+def p_chunk1(p):
+    '''chunk : LROUND RROUND
+             | LSQUARE RSQUARE
+             | LANGLE RANGLE
+             | LCURLY RCURLY
+    '''
+    p[0] = p[1] + p[2]
+
+
+def p_chunk2(p):
+    '''chunk : LROUND chunks RROUND
+             | LSQUARE chunks RSQUARE
+             | LANGLE chunks RANGLE
+             | LCURLY chunks RCURLY
+    '''
+    p[0] = p[1] + p[2] + p[3]
+
+
+precedence = (
+    ('left', 'LROUND'),
+    ('left', 'RROUND'),
+    ('left', 'LSQUARE'),
+    ('left', 'RSQUARE'),
+    ('left', 'LANGLE'),
+    ('left', 'RANGLE'),
+    ('left', 'LCURLY'),
+    ('left', 'RCURLY'),
+    ('left', 'NEWLINE'),
+)
+
+
+chunk_errors = []
+
+
+incomplete = []
+corrupted = []
+# should be: 3, 5, 6, 8, 9
+
+
+def p_error(p):
+    if p is None:
+        raise Exception("Unexpected end of file.")
+    msg = f"Syntax error! Error on token: {repr(p.value)} ({p.type})"
+    make_error(p, "error", msg)
+
+
+def make_error(p, type, message):
+    column = find_column(p.lexer.lexdata, p)
+    form = "{path}:{line}:{column}: {type}: {msg}"
+    formatted = form.format(path=FILENAME, line=p.lineno, column=column, type=type, msg=message)
+    if p.type == "NEWLINE":
+        incomplete.append((p.lineno, column, p.type, [x for x in parser.symstack]))
+        parser.restart()
+    else:
+        corrupted.append((p.lineno, column, p.type))
+    if LINT:
+        print(formatted)
+    else:
+        raise Exception(formatted)
+
+
+parser = yacc.yacc()
+
+
+def do_parsing(text):
+    result = parser.parse(text)
+    return result
