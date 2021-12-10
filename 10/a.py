de67b50be999afca0588d8e053e4d79633aa41ae
Marco Schlicht <git@privacymail.dev>
2021-12-10 23:18:06 +0100

    improved

--- 10/a.py
+++ 10/a.py
@@ -12,6 +12,5 @@ if __name__ == '__main__':
     point_map = {'RROUND': 3, 'RSQUARE': 57, 'RCURLY': 1197, 'RANGLE': 25137}
     # reverse order to overwrite with the first item at last
     # filters the first element of every line
-    corrupted_first = {x: (y, z) for x, y, z in corrupted[::-1]}  # XXX
-    corrupted_tokens = [item[1][1] for item in corrupted_first.items()]
-    print(sum(map(lambda item: point_map[item], corrupted_tokens)))
+    corrupted_first = {x: z for x, _, z in corrupted[::-1]}  # XXX
+    print(sum(map(point_map.get, corrupted_first.values())))
