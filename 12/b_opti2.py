232fda1e0fd9ddd66cc64fe4401dca76f8247f27
Marco Schlicht <git@privacymail.dev>
2021-12-12 23:46:30 +0100

    optimized b

--- 12/b_opti2.py
+++ 12/b_opti2.py
@@ -1,25 +1,33 @@
 from __future__ import annotations
 import time
 from pprint import pprint
-from typing import List
+from typing import List, Dict, Tuple
 
 inp = [line.split("-") for line in open("input").read().splitlines()]
 
 
-def can_continue(node: str, du_kommst_von: List[str]):
+counts = {}
+for x, y in inp:
+    if x not in counts.keys():
+        counts[x] = [0, x.islower()]
+    if y not in counts.keys():
+        counts[y] = [0, y.islower()]
+
+
+def can_continue(node: str, du_kommst_von: Dict[str, List[int, bool]]):
     if node.isupper():
         return True
-    if du_kommst_von.count(node) < 1:
+    if du_kommst_von[node][0] < 1:
         return True
-    for item in du_kommst_von:
-        if item.islower() and du_kommst_von.count(item) > 1:
+    for count, lower in du_kommst_von.values():
+        if lower and count > 1:
             return False
     if node not in ['start', 'end']:
         return True
     return False
 
 
-def finde_nodes(node: str, du_kommst_von: List[str]):
+def finde_nodes(node: str, du_kommst_von: Dict[str, List[int, bool]]):
     for x, y in inp:
         if x == node:
             if can_continue(y, du_kommst_von):
@@ -29,20 +37,20 @@ def finde_nodes(node: str, du_kommst_von: List[str]):
                 yield x
 
 
-def suche(node: str, du_kommst_von: List[str]):
-    du_kommst_von.append(node)
+def suche(node: str, du_kommst_von: Dict[str, List[int, bool]]):
     if node == "end":
         return 1
+    du_kommst_von[node][0] += 1
     new_nodes = finde_nodes(node, du_kommst_von)
     count = 0
     for new_node in new_nodes:
         count += suche(new_node, du_kommst_von)
-        du_kommst_von.pop(-1)
+    du_kommst_von[node][0] -= 1
     return count
 
 
 def b():
-    count = suche('start', [])
+    count = suche('start', counts)
     print(count)
 
 
